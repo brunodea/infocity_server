b36059200e9cf071836e7e608e4339b37ff964a3
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-10-23 21:02:56 -0200

    decoupled the urlsConf. The events has its own now.

--- infocity/urls.py
+++ infocity/urls.py
@@ -5,11 +5,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^infocity/events/$', 'events.views.events'),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^events/', include('events.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
