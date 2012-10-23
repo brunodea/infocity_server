eea88c8c9f18eca329a6b7a1d9f650f7913448b9
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-10-23 20:54:29 -0200

    description for the method 'events' from events.view added.

--- events/views.py
+++ events/views.py
@@ -2,4 +2,12 @@ from models import EventType, Event
 from django.http import HttpResponse
 
 def events(request):
+    """
+    Key method that delegates the response to another method accordingly some
+    request attribute(s).
+    """
+    if request.method == 'GET':
+        pass
+    if request.method == 'POST':
+        pass
     return HttpResponse('test')
