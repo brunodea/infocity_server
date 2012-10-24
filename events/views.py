7e9bc0711e4f658fe688cc307a77649bdcfc9d9a
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-10-23 22:07:38 -0200

    some expermentations in views.

--- events/views.py
+++ events/views.py
@@ -1,13 +1,21 @@
 from models import EventType, Event
 from django.http import HttpResponse
 
+import jsonhelper
+
 def events(request):
     """
     Key method that delegates the response to another method accordingly some
     request attribute(s).
     """
+    event_name = 'generic event type'
     if request.method == 'GET':
-        pass
+        if 'name' in request.GET:
+            event_name = request.GET['name']
+        else:
+            event_name = 'get event type'
     if request.method == 'POST':
-        pass
-    return HttpResponse('test')
+        event_name = 'post event type'
+
+    response = jsonhelper.toJSON(EventType(name=event_name))
+    return HttpResponse(str(response))
