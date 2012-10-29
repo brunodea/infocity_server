703cd635e486db9dea3e0914645f371c0c0bcac9
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-10-29 20:03:45 -0200

    adicionei o gis no INSTALLED_APPS de settings.py.

--- infocity/settings.py
+++ infocity/settings.py
@@ -119,7 +119,8 @@ INSTALLED_APPS = (
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
-    'events'
+     'events',
+     'django.contrib.gis'
 )
 
 # A sample logging configuration. The only tangible logging
