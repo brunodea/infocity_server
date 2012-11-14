0a0f9e0fd4a3e395069e46d9f3a5d8a067dafb24
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-14 20:42:10 -0200

    adicionei model EventKeyword e método __unicode__ para os models.

--- events/models.py
+++ events/models.py
@@ -5,12 +5,27 @@ from django.contrib.gis.db import models
 class EventType(models.Model):
     name = models.CharField(max_length=64)
 
+    def __unicode__(self):
+        return str(self.name)
+
+class EventKeyword(models.Model):
+    keyword = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return str(keyword)
+
 class Event(models.Model):
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=300)
     event_type = models.ForeignKey(EventType)
     pub_date = models.DateTimeField('date published')
-    
+    keywords = models.ManyToManyField(EventKeyword)
+
     #GeoDjango específico.
     geo_coord = models.PointField()
     objects = models.GeoManager()
+    
+    def __unicode__(self):
+        return "[%s]%s (%s) : %s\n%s\n%s" % (str(self.geo_coord),
+            str(self.title), str(self.event_type), str(self.pub_date),
+            str(self.description), 'keywords...')
