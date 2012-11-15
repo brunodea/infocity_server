44555e258bf51b8548548bd03bd5145bd2f53259
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-15 21:08:30 -0200

    tela do admin mostrando os models que faltavam.

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,6 @@
-from events.models import EventType
-from django.contrib import admin
+from events.models import EventType, EventKeyword, Event
+from django.contrib.gis import admin
 
+admin.site.register(Event, admin.OSMGeoAdmin)
 admin.site.register(EventType)
+admin.site.register(EventKeyword)
