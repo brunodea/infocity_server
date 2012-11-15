006a5d1dfbf1ee1c1bf46b343237e5742df49e5b
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-15 21:10:40 -0200

    método de salvar o evento no BD alterado.

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
-from models import EventType, Event
+from models import EventType, Event, EventKeyword
 from django.http import HttpResponse
 from django.core.serializers.base import DeserializationError
+from django.contrib.gis.geos import GEOSGeometry
 
 import jsonhelper
 
@@ -27,10 +28,29 @@ def addNewEvent(request):
     """
     response = {'response':'ok'}
     try:
-        event_json = request.POST['event'].encode('utf-8')
-        items = jsonhelper.fromJSON(event_json, True)
-        for item in items:
-            item.save()
+    
+        event_type_name = request.POST['event_type'].encode('utf-8').lower().strip()
+        event_type, created = EventType.objects.get_or_create(name=event_type_name)
+    
+        event_keywords = request.POST['event_keywords'].encode('utf-8')
+        event_keywords = event_keywords[1:-1].split(',')
+        keywords = []
+        for keyword in event_keywords:
+            k, created = EventKeyword.objects.get_or_create(keyword=keyword.strip())
+            keywords.append(k)
+
+        event_json = eval(request.POST['event'].encode('utf-8'))[0]['fields']
+
+        geo_coord = event_json['geo_coord'].replace(',',' ')
+        geo_coord = GEOSGeometry('POINT('+geo_coord+')')
+
+        event = Event(title=event_json['title'],description=event_json['description'],
+            pub_date=event_json['pub_date'],geo_coord=geo_coord)
+        event.event_type = event_type
+        event.save()
+        for k in keywords:
+            event.keywords.add(k)
+        event.save()
     except DeserializationError as error:
         response['response'] = str(error)
     return HttpResponse(str(response),'application/json')
