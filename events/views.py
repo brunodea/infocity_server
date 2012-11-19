7da6d78628b6f86288d055d0504b35f6903d7967
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-19 20:56:17 -0200

    removi método inutil de views.

--- events/views.py
+++ events/views.py
@@ -5,23 +5,6 @@ from django.contrib.gis.geos import GEOSGeometry
 
 import jsonhelper
 
-def events(request):
-    """
-    Key method that delegates the response to another method accordingly some
-    request attribute(s).
-    """
-    event_name = 'generic event type'
-    if request.method == 'GET':
-        if 'name' in request.GET:
-            event_name = request.GET['name']
-        else:
-            event_name = 'get event type'
-    if request.method == 'POST':
-        event_name = 'post event type'
-
-    response = jsonhelper.toJSON(EventType(name=event_name))
-    return jsonhelper.json_response(response)
-
 def addNewEvent(request):
     """
     Method that adds an event to the spatial database.
