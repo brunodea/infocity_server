fc53e18836174c4da413c46d53e1d8c932b45957
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-19 20:52:04 -0200

    método jsonhelper.toJSON() aceitando qualquer tipo de parâmetro.

--- events/jsonhelper.py
+++ events/jsonhelper.py
@@ -1,16 +1,16 @@
 from django.core.serializers import serialize, deserialize
 from django.db import models
 from django.db.models.query import QuerySet
+from django.utils import simplejson
+from django.http import HttpResponse
 
 def toJSON(obj):
     if isinstance(obj, QuerySet):
         pass
     elif isinstance(obj, models.Model):
-        set_obj = [obj]
-    else:
-        return None
+        obj = [obj]
 
-    ser = serialize('json',set_obj,ensure_ascii=False)
+    ser = serialize('json', obj, ensure_ascii=False)
     set_str = ser[1:len(ser)-1]
     return set_str
 
