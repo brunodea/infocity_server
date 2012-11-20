aa4cc857a637a4d117e4ff164e70936b5908e10f
Bruno Azevedo <brunodea@inf.ufsm.br>
2012-11-20 18:52:38 -0200

    removi events/ das urls já que chamava a função inutilizada e recém removida.

--- events/urls.py
+++ events/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('events.views',
-    url(r'^events/$', 'events'),
     url(r'^add/$', 'addNewEvent'),
     url(r'^getWithin/(?P<latitude>-?\d+(\.\d+)?)/(?P<longitude>-?\d+(\.\d+)?)' +
         '/(?P<distance_meters>\d+(\.\d+)?)/(?P<discard_pks>(\d+/)*)$', 'getEventsWithin'),
